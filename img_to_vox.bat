b6d67c678400edfbdf8e46d979840139e45e0653
liamlangli <lilang02@corp.netease.com>
2026-09-01 14:55:15 +0800

    ignore

--- img_to_vox.bat
+++ img_to_vox.bat
@@ -8,5 +8,5 @@ if not exist ".venv\Scripts\activate.bat" (
 )
 
 call ".venv\Scripts\activate.bat"
-python img_to_vox.py --input_folder "C:\Users\lilang02\Downloads\vox" --skip
+python img_to_vox.py --input_folder "C:\code\TRELLIS.2\vox" --skip
 exit /b %ERRORLEVEL%
